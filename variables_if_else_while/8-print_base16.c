f6f2824752f95ac390581315688ee524c5f69057
meenakshee23 <meenaksheegopaul@gmail.com>
2025-06-26 19:26:54 +0400

    New file

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -10,13 +10,12 @@ int main(void)
 	int i;
 
 	for (i = 0; i < 10; i++)
-		putchar(i + 0);
+		putchar(i + '0');
 
-	for (i + 0; i < 6; i++)
-		putchar(i + a);
+	for (i = 0; i < 6; i++)
+		putchar(i + 'a');
 
 	putchar('\n');
 
 	return (0);
 }
-
