9bf19222e0aa636a52db89ffd5d9c946aa0d3403
meenakshee23 <meenaksheegopaul@gmail.com>
2025-07-03 14:23:56 +0400

    new file

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts a string to an integer
+ * @s: The string to convert
+ *
+ * Return: The converted integer
+ */
+int _atoi(char *s)
+{
+	int i = 0, sign = 1, result = 0;
+
+	while (s[i])
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			result = result * 10 + (s[i] - '0');
+			i++;
+			while (s[i] >= '0' && s[i] <= '9')
+			{
+				result = result * 10 + (s[i] - '0');
+				i++;
+			}
+			break;
+		}
+		i++;
+	}
+
+	return (sign * result);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,7 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c);
 char *_strcpy(char *dest, char *src);
-int _putchar(char c); 
+int _atoi(char *s);
+void puts2(char *str);
+void puts_half(char *str);
 
 #endif
