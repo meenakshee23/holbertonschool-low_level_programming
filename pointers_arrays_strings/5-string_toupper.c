bc241e9fbabef41d870307e83bc9111c10bc6efe
meenakshee23 <meenaksheegopaul@gmail.com>
2025-07-05 22:16:25 +0400

    new file

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *
+ *char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 'a' + 'A';
+		i++;
+	}
+	return (str);
+}
