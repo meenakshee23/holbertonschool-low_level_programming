d7a9a8fe835937f58936dbc359fa55b1fa36cde5
meenakshee23 <meenaksheegopaul@gmail.com>
2025-07-16 11:44:37 +0400

    new file

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup -pointer to a new string which is a duplicate of the string
+ * @str: the original string
+ *
+ * Return: NULL if str = NULL
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int len = 0;
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len] != '\0')
+		len++;
+	s = malloc(sizeof(char) * (len + 1));
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+		s[i] = str[i];
+
+	return (s);
+}
