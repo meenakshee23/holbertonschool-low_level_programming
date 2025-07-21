1d1d77b321f1623890cb98da5e1dced2bc0c2257
meenakshee23 <meenaksheegopaul@gmail.com>
2025-07-22 00:06:55 +0400

    new file

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - frees memory of a dog_t
+ * @d: pointer to dog_t to free
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
