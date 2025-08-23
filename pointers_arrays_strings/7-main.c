8ef840cc4cf3034a9b9c5231f446a68240d93f09
meenakshee23 <meenaksheegopaul@gmail.com>
2025-08-24 01:18:49 +0400

    new file

--- pointers_arrays_strings/7-main.c
+++ pointers_arrays_strings/7-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - check the code
@@ -8,16 +7,9 @@
  */
 int main(void)
 {
-    char board[8][8] = {
-        {'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
-        {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-        {'R', 'K', 'B', 'Q', 'K', 'B', 'K', 'R'},
-    };
-    print_chessboard(board);
+    char *str;
+
+    str = "0123456789";
+    puts_half(str);
     return (0);
 }
--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,18 +1,20 @@
 #include "main.h"
-
 /**
- * puts_half - Prints the second half of a string.
- * @str: The string to print.
+ * puts_half - prints the second half of a string.
+ * @str: the string to print.
  */
 void puts_half(char *str)
 {
-	int i = 0;
+	int length = 0;
 	int n;
 
-	while (str[i] != '\0')
-		i++;
+	while (str[length] != '\0')
+		length++;
 
-	n = (i % 2 == 0) ? (i / 2) : ((i + 1) / 2);
+	if (length % 2 == 0)
+		n = length / 2;
+	else
+		n = (length + 1) / 2;
 
 	while (str[n] != '\0')
 	{
