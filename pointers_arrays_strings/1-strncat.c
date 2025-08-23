e6f6a01119031d71f5d1a6689ad38e3db3e5f1dd
meenakshee23 <meenaksheegopaul@gmail.com>
2025-08-24 03:49:32 +0400

    my file

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,23 +1,19 @@
 #include "main.h"
-
 /**
- * _strncat - concatenates two strings using at most n bytes from src
+ * _strncat - concatenates two strings
  * @dest: destination string
  * @src: source string
- * @n: maximum number of bytes from src to use
+ * @n: max bytes to append
  *
- * Return: pointer to the resulting string dest
+ * Return: pointer to dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0;
-	int j = 0;
+	int i = 0, j = 0;
 
-	/* Find the end of dest */
 	while (dest[i] != '\0')
 		i++;
 
-	/* Append at most n bytes from src */
 	while (j < n && src[j] != '\0')
 	{
 		dest[i] = src[j];
@@ -25,8 +21,8 @@ char *_strncat(char *dest, char *src, int n)
 		j++;
 	}
 
-	/* Null-terminate dest */
 	dest[i] = '\0';
 
 	return (dest);
 }
+
