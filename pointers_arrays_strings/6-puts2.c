a7ed8faa2ee6d33829b89a373028bd4268724e8d
meenakshee23 <meenaksheegopaul@gmail.com>
2025-08-24 01:06:06 +0400

    new file

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * puts2 - Prints every other character of a string,
+ * puts2 - prints every other character of a string,
  * starting with the first character, followed by a new line.
- * @str: The input string.
+ * @str: the input string.
  */
 void puts2(char *str)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (str[i] != '\0')
 	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
+		_putchar(str[i]);
+		i += 2;
 	}
 	_putchar('\n');
 }
+
