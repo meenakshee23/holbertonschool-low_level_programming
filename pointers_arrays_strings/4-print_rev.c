25de8c5a71022fbbc41d4a0649f51e8701294dfc
meenakshee23 <meenaksheegopaul@gmail.com>
2025-08-24 00:14:47 +0400

    new file

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,24 +1,23 @@
 #include "main.h"
-
 /**
  * print_rev - prints a string in reverse, followed by a new line
  * @s: pointer to the string to be reversed
  */
 void print_rev(char *s)
 {
-	int len = 0;
+	int i  = 0;
 
-	/* First, find the length of the string */
-	while (s[len] != '\0')
+
+	while (s[i] != '\0')
 	{
-		len++;
+		i++;
 	}
 
-	/* Print characters in reverse */
-	while (len > 0)
+
+	while (i  > 0)
 	{
-		len--;
-		_putchar(s[len]);
+		i--;
+		_putchar(s[i]);
 	}
 
 	_putchar('\n');
