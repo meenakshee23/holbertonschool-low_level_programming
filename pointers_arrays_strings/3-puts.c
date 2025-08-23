519a6160e5452af5422e4ec47d38bdd3461aba3d
meenakshee23 <meenaksheegopaul@gmail.com>
2025-08-23 23:48:21 +0400

    new file

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,8 +1,7 @@
 #include "main.h"
-
 /**
- * _puts - prints a string, followed by a new line, to stdout
- * @str: pointer to the string to print
+ * _puts - prints a string followed by a new line
+ * @str: the string to print
  */
 void _puts(char *str)
 {
@@ -11,7 +10,8 @@ void _puts(char *str)
 	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
-		i++;
+			i++;
 	}
 	_putchar('\n');
 }
+
