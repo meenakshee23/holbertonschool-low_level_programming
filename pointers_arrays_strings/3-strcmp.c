0594ba32378966026436eae3e76f51dec962bf59
meenakshee23 <meenaksheegopaul@gmail.com>
2025-08-24 13:27:22 +0400

    my file

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,21 +1,17 @@
 #include "main.h"
-
 /**
  * _strcmp - compares two strings
  * @s1: first string
  * @s2: second string
  *
- * Return: an integer less than, equal to, or greater than 0 if s1 is found,
- * respectively, to be less than, to match, or be greater than s2
+ * Return: the difference between the first unmatched characters
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2)
-	{
-		if (*s1 != *s2)
-			return (*s1 - *s2);
-		s1++;
-		s2++;
-	}
-	return (*s1 - *s2);
+	int i = 0;
+
+	while (s1[i] == s2[i] && s1[i] != '\0')
+		i++;
+
+	return (s1[i] - s2[i]);
 }
