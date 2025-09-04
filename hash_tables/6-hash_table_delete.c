fb229ec87bfacf959768b157436b75a0fc3d6f67
meenakshee23 <meenaksheegopaul@gmail.com>
2025-09-04 23:26:45 +0400

    new file

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: pointer to the hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node, *temp;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			temp = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = temp;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
