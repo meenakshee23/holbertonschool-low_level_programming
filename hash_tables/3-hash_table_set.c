feb0b2b1b8f91bd53e71f33125bf829370136ffd
meenakshee23 <meenaksheegopaul@gmail.com>
2025-09-04 21:10:18 +0400

    new file

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,33 +1,63 @@
 #include "hash_tables.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-char *strdup(const char *s);
+/**
+ * _strdup - custom implementation of strdup
+ * @str: string to duplicate
+ * Return: pointer to new string or NULL
+ */
+char *_strdup(const char *str)
+{
+	char *dup;
+	size_t len = 0, i;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len])
+		len++;
+
+	dup = malloc(len + 1);
+	if (!dup)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+		dup[i] = str[i];
+
+	return (dup);
+}
 /**
  * hash_table_set - adds an element to the hash table
- * @ht: pointer to the hash table
+ * @ht: the hash table you want to add or update the key/value
  * @key: key can not be an empty string
- * @value: the va;ue associated with the key. value can be an empty string
+ * @value: the value associated with the key
  *
  * Return: 1 if it succeeded, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_node, *current;
+	hash_node_t *current;
+	hash_node_t *new_node;
+	char *new_value;
 
-	if (!ht || !key || key[0] == '\0' || !value)
+	if (ht == NULL || key == NULL || key[0] == '\0' || value == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-
 	current = ht->array[index];
+
 	while (current)
 	{
 		if (strcmp(current->key, key) == 0)
 		{
-			free(current->value);
-			current->value = strdup(value);
-			if (!current->value)
+			new_value = _strdup(value);
+			if (new_value == NULL)
 				return (0);
+
+			free(current->value);
+			current->value = new_value;
 			return (1);
 		}
 		current = current->next;
@@ -37,9 +67,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!new_node)
 		return (0);
 
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-	if (!new_node->key || !new_node->value)
+	new_node->key = _strdup(key);
+	new_node->value = _strdup(value);
+	if (new_node->key == NULL || new_node->value == NULL)
 	{
 		free(new_node->key);
 		free(new_node->value);
@@ -49,5 +79,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
+
 	return (1);
 }
