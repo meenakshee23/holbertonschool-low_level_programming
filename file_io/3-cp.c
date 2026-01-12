03cd1f675a778263235d8077f2e064e5715df37b
meenakshee23 <meenaksheegopaul@gmail.com>
2026-01-13 00:31:53 +0400

    new file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,27 +1,66 @@
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int main(int ac, char **av)
 {
 	int fd_from, fd_to;
 	char buffer[1024];
-	ssize_t bytes;
+	ssize_t bytes, w_bytes;
 
 	if (ac != 3)
-		return (1);
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from < 0)
-		return (1);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
 
 	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to < 0)
-		return (1);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		close(fd_from);
+		exit(99);
+	}
 
 	while ((bytes = read(fd_from, buffer, 1024)) > 0)
-		write(fd_to, buffer, bytes);
+	{
+		w_bytes = write(fd_to, buffer, bytes);
+		if (w_bytes != bytes)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			close(fd_from);
+			close(fd_to);
+			exit(99);
+		}
+	}
 
-	close(fd_from);
-	close(fd_to);
+	if (bytes < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		close(fd_from);
+		close(fd_to);
+		exit(98);
+	}
+
+	if (close(fd_from) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+
+	if (close(fd_to) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
+
 	return (0);
 }
